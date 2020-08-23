19c1ca45a245921e8c86fcfb85ab906bf2dffcf1
fang duan <df1228@gmail.com>
2020-08-23 15:22:25 +0800

    add translation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react'
+import { Translations } from '@aws-amplify/ui-components'
+import { I18n } from '@aws-amplify/core';
+
+// https://github.com/aws-amplify/amplify-js/blob/main/packages/amplify-ui-components/src/common/Translations.ts
+// amplify-js/packages/aws-amplify-react/src/AmplifyI18n.tsx
+I18n.putVocabulariesForLanguage('zh', {
+  [Translations.SIGN_IN_ACTION]: '登录',
+});
+
+
+I18n.setLanguage('zh');
 
 function App() {
   return (
